091007b8c6e0f120b38833d5f6cdc547b4a1a15b
Tiago Ezidio <tiago@simetria.studio>
2021-11-30 14:22:02 -0300

    Auto stash before merge of "main" and "origin/main"

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -5,11 +5,11 @@
         <div class="ola d-md-flex mt-5 ">
             <div class="nome-usuario">
                 <img class="img-0" src="{{ url('assets/img/hi-mobbin-150x150.png') }}" alt="">
-                <p><span style="color: rgb(79, 217, 214);">Olá</span>, {{ auth()->user()->name }}</p>
+                <p>Olá, <span style="color: #ffa800;">{{ auth()->user()->name }}</span></p>
             </div>
             <div class="anuncio ">
                 <div class="banner">
-                    <p>Procurando <br> jobs?</p>
+                    <img src="{{ url('assets/img/banner.jpg') }}" alt="">
                 </div>
             </div>
         </div>
